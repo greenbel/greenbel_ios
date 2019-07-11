a90ce7d27d2079d83d4f9d170bfc8e24ab74ede0
Kiryl Klimovich <kirylklimovich@gmail.com>
2019-07-11 16:13:48 +0300

    Start swift app

---
+++ GreenBel/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GreenBel
+//
+//  Created by Kiryl Klimovich on 7/11/19.
+//  Copyright © 2019 Kiryl Klimovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var welcome_label: UILabel!
+    @IBOutlet weak var moto: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
